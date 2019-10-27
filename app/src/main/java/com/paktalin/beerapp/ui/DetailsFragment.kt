ee36074ad46d4c02fe9b609203b952855e1c31ab
likorn <likorn@ttu.ee>
2019-10-27 03:33:05 +0300

    Details sections are expandable

--- app/src/main/java/com/paktalin/beerapp/ui/DetailsFragment.kt
+++ app/src/main/java/com/paktalin/beerapp/ui/DetailsFragment.kt
@@ -6,15 +6,21 @@ import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.paktalin.beerapp.Beer
 import com.paktalin.beerapp.R
 import com.paktalin.beerapp.addToFavorite
 import com.paktalin.beerapp.removeFromFavorite
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_detais.view.*
+import kotlinx.android.synthetic.main.layout_brewers_tips.view.*
+import kotlinx.android.synthetic.main.layout_characteristics.view.*
+import kotlinx.android.synthetic.main.layout_food_pairing.view.*
+
 
 class DetailsFragment : Fragment() {
 
@@ -35,18 +41,46 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_detais, container, false)
-        (activity as AppCompatActivity).setSupportActionBar(view.toolbar as Toolbar)
+        with(view) {
+            (activity as AppCompatActivity).setSupportActionBar(toolbar as Toolbar)
+            collapsing_toolbar.title = beer.name
+            Picasso.with(context).load(beer.imageUrl).into(image_beer)
 
-        view.collapsing_toolbar.title = beer.name
+            setUpColors(view)
+            setUpButtonFavorite(button_add_to_favorite)
+            setUpExpandButtons(view)
+        }
+        return view
+    }
 
+    private fun setUpExpandButtons(view: View) {
+        with(view) {
+            button_expand_characteristics.isSelected = true
+            button_expand_characteristics.setExpandableButton(characteristics_expanded)
+            button_expand_brewers_tips.setExpandableButton(brewers_tips_expanded)
+            button_expand_food_pairing.setExpandableButton(food_pairing_expanded)
+        }
+    }
+
+    private fun ImageButton.setExpandableButton(expandableView: View) {
+        setOnClickListener {
+            it.isSelected = !it.isSelected
+            expandableView.visibility = if (it.isSelected) View.VISIBLE else View.GONE
+            expandableView.parent.requestChildFocus(expandableView, expandableView)
+        }
+    }
+
+    private fun setUpColors(view: View) {
         beer.colorSet?.apply {
             view.collapsing_toolbar.setExpandedTitleTextColor(ColorStateList.valueOf(textColor))
             view.collapsing_toolbar.setCollapsedTitleTextColor(textColor)
             view.view_color?.setBackgroundColor(backgroundColor)
             view.collapsing_toolbar.setContentScrimColor(backgroundColor)
         }
+    }
 
-        with(view.button_add_to_favorite) {
+    private fun setUpButtonFavorite(button: FloatingActionButton) {
+        with(button) {
             isSelected = beer.isFavorite
             setOnClickListener {
                 if (!isSelected)
@@ -57,10 +91,5 @@ class DetailsFragment : Fragment() {
                 isSelected = !isSelected
             }
         }
-
-        Picasso.with(context)
-            .load(beer.imageUrl)
-            .into(view.image_beer)
-        return view
     }
 }
